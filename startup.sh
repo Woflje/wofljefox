5d0f9afb436f261af9d152814db9411f6a189a13
Woflje <thelegendofwolfi@gmail.com>
2021-07-04 20:15:27 +0200

    changed directory name to bin

--- startup.sh
+++ startup.sh
@@ -1,2 +1,2 @@
-cd /home/woflje/wofljefox
-screen -S wofljefox -d -m /usr/bin/python3 /home/woflje/wofljefox/main.py
+cd /home/woflje/wofljefox/bin
+screen -S wofljefox -d -m /usr/bin/python3 /home/woflje/wofljefox/bin/main.py
